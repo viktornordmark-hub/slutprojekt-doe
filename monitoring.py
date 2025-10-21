869071cff2d1dfb29a54310c0a5a80f497a2626a
viktornordmark-hub <viktornordmark@gmail.com>
2025-10-21 17:57:43 +0200

    Created loop for system snapshot

--- monitoring.py
+++ monitoring.py
@@ -1,45 +1,36 @@
 '''System snapshot and setting monitoring'''
-import time
 import psutil
 from functions import bytes_to_gb
 from functions import check_os
 #from functions import print_separator
 from functions import wait_any_key
+from functions import clean_terminal
 
 def system_snapshot():
     '''Take snapshot of system load'''
-    cpu_usage = psutil.cpu_percent(interval=1)
-    disk_info = psutil.disk_usage(check_os())
-    ram_info = psutil.virtual_memory()
-
-    #convert bytes to GB
-    used_d = bytes_to_gb(disk_info.used)
-    total_d = bytes_to_gb(disk_info.total)
-    used_ram = bytes_to_gb(ram_info.used)
-    total_ram = bytes_to_gb(ram_info.total)
-
-    # Top frame
-    #print_separator(50)
     snapshot = True
     while snapshot:
-        
+        cpu_usage = psutil.cpu_percent(interval=1)
+        disk_info = psutil.disk_usage(check_os())
+        ram_info = psutil.virtual_memory()
+
+        used_d = bytes_to_gb(disk_info.used)
+        total_d = bytes_to_gb(disk_info.total)
+        used_ram = bytes_to_gb(ram_info.used)
+        total_ram = bytes_to_gb(ram_info.total)
+
+        clean_terminal()
         print(f"CPU-användning: {cpu_usage}% |")
         print(f"Diskanvändning: {disk_info.percent}% | "
                 f"{used_d} GB / {total_d} GB.")
         print(f"RAM-användning: {ram_info.percent}% | "
                 f"{used_ram} GB / {total_ram} GB.")
-        if wait_any_key():
+        print("Press any key to return to menu...")
+        if wait_any_key(non_blocking=True, timeout=3):
             snapshot = False
+            break
         else:
             continue
-        time.sleep(3)
-
-
-    # Bottom frame
-    #print_separator(50)
-
-    return
-#system_snapshot()
 
 def start_monitoring():
     '''Set monitoring to True'''
