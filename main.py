9b364b777a079964c8ce60a1d4f7c298011b2f21
viktornordmark-hub <viktornordmark@gmail.com>
2025-10-21 18:03:13 +0200

    Wrote it in english and finished the menu

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from menu import menu_1
 from menu import menu_2
 from menu import menu_3
 from menu import menu_4
-
+from menu import menu_5
+from functions import clean_terminal
 
 
 def run_menu():
@@ -13,12 +14,12 @@ def run_menu():
     input_menu = True
     while input_menu:
         menu_choice = input(
-            "1. Starta övervakning\n"
-            "2. Lista aktiv övervakning\n"
-            "3. Skapa larm\n"
-            "4. Visa larm\n"
-            "5. Starta övervakningsläge\n"
-            "6. Avsluta\n")
+            "1. Start monitoring\n"
+            "2. List active monitoring\n"
+            "3. Create larm\n"
+            "4. Show larm\n"
+            "5. Start monitor mode\n"
+            "6. Exit\n")
         match menu_choice:
             case '1':
                 set_monitoring = menu_1(set_monitoring)
@@ -29,7 +30,12 @@ def run_menu():
             case '4':
                 menu_4(alarm_list)
             case '5':
+                menu_5(alarm_list)
+            case '6':
+                clean_terminal()
+                print("Program exits.")
                 break
             case _:
-                break
+                clean_terminal()
+                print("Please choose 1-6!")
 run_menu()
